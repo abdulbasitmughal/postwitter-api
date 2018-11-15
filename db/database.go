d28bb59b905cfb572155bc64abd6f241a783839c
Abdul Basit <abdulbasitmughal@gmail.com>
2018-11-15 09:24:42 +0500

    fix in post message api

--- db/database.go
+++ db/database.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"database/sql"
+	"os"
+	"strings"
 
 	// load drivers for mysql
 	_ "github.com/go-sql-driver/mysql"
@@ -10,19 +12,19 @@ import (
 // CreateCon mysql database connection
 func CreateCon() *sql.DB {
 
-	// dbURI := os.Getenv("CLEARDB_DATABASE_URL")
-	// dbURIParts := strings.Split(dbURI, "//")[1]
+	dbURI := os.Getenv("CLEARDB_DATABASE_URL")
+	dbURIParts := strings.Split(dbURI, "//")[1]
 
-	// dbURISubParts := strings.Split(dbURIParts, "@")
-	// domain := dbURISubParts[1]
-	// dbURIPartAddress := strings.Split(domain, "/")
-	// dbName := dbURIPartAddress[1]
-	// dbNameString := strings.Split(dbName, "?")
+	dbURISubParts := strings.Split(dbURIParts, "@")
+	domain := dbURISubParts[1]
+	dbURIPartAddress := strings.Split(domain, "/")
+	dbName := dbURIPartAddress[1]
+	dbNameString := strings.Split(dbName, "?")
 
 	// fmt.Println(dbURIParts[1])
 
-	// db, err := sql.Open("mysql", dbURISubParts[0]+"@tcp("+dbURIPartAddress[0]+":3306)/"+dbNameString[0])
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_twitter")
+	db, err := sql.Open("mysql", dbURISubParts[0]+"@tcp("+dbURIPartAddress[0]+":3306)/"+dbNameString[0])
+	// db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_twitter")
 	if err != nil {
 		panic(err)
 	}
